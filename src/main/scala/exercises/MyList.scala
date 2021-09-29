c15497460b8e170ae4811f73644e7d8c15e257cd
micksadowsky <micsad999@gmail.com>
2021-09-29 01:46:59 +0100

    L18 Generics

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
   /*
   * head = 1st element of the list
   * tail = remainder of the list
@@ -9,32 +9,31 @@ abstract class MyList {
   * toString = a string representation of the list
   */
 
-//  val listElements: Array[Int]
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String = {
     if (t.isEmpty) "" + h
-    else h + " " + t.printElements
+    else "" + h + " " + t.printElements
   }
 }
 
@@ -44,4 +43,9 @@ object ListTest extends App {
   println(list.add(4).head)
   println(list.isEmpty)
   println(list.toString)
+
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val lisstOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+  println(listOfIntegers.toString)
+  println(lisstOfStrings.toString)
 }
