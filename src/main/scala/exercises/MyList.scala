093e8b4a81652f597512c18b96fb5b6b04695e0c
micksadowsky <micsad999@gmail.com>
2021-10-05 22:35:07 +0100

    L27&L28&L29&L30&L31 Functional Programming continued

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -24,6 +24,13 @@ abstract class MyList[+A] {
 
   // concatenation
   def ++[B >: A](list: MyList[B]): MyList[B]
+
+  // HOFs
+  def foreach(f: A => Unit): Unit
+  def sort(compare: (A,A) => Int): MyList[A]
+  def zipWith[B, C](list: MyList[B], zip:(A, B) => C): MyList[C]
+  def fold[B](start: B)(operator: (B, A) => B) : B
+
 }
 
 case object Empty extends MyList[Nothing] {
@@ -38,6 +45,16 @@ case object Empty extends MyList[Nothing] {
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+
+  // HOFs
+  def foreach(f: Nothing => Unit): Unit = ()
+  def sort(compare: (Nothing, Nothing) => Int) = Empty
+  def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] =
+    if (!list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else Empty
+
+  def fold[B](start: B)(operator: (B, Nothing) => B): B  = start
+
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -90,6 +107,36 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   */
   def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 
+  // HOFs
+  def foreach(f: A => Unit): Unit = {
+    f(h)
+    t.foreach(f)
+  }
+
+  def sort(compare: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+    }
+
+    val sortedTail = t.sort(compare)
+    insert(h, sortedTail)
+  }
+
+  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] =
+    if (list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else new Cons(zip(h, list.head), t.zipWith(list.tail, zip))
+
+  // [1,2,3].fold(0)(+)
+  // = [2,3].fold(1)(+)
+  // = [3].fold(3)(+)
+  // = [].fold(6)(+)
+  // = 6
+  def fold[B](start: B)(operator: (B, A) => B): B = {
+    t.fold(operator(start, h))(operator)
+
+  }
 }
 
 // 1. Generic trait MyPredicate[-T]
@@ -139,5 +186,26 @@ object ListTest extends App {
   println(listOfIntegers.flatMap(elem => new Cons(elem, new Cons(elem+1, Empty))).toString)
 
   println(cloneListOfIntegers == listOfIntegers) // = true
+
+  listOfIntegers.foreach(println)
+  println(listOfIntegers.sort((x, y) => y - x))
+  println(anotherListOfIntegers.zipWith[String, String](lisstOfStrings, _ + "-" + _))
+  println(listOfIntegers.fold(0)(_+_))
+
+  // for comprehensions
+  val combinations = for {
+    n <- listOfIntegers
+    string <- lisstOfStrings
+  } yield n + "-" + string
+
+  println(combinations)
+  println(
+    for {
+      n <- listOfIntegers
+      string <- lisstOfStrings
+    } yield n + "-" + string
+  )
+
+
 
 }
---
+++ src/main/scala/lectures/part3fp/Sequences.scala
@@ -0,0 +1,80 @@
+package lectures.part3fp
+
+import scala.util.Random
+
+object Sequences extends App {
+
+  // Seq
+  val aSequence = Seq(1,3,2,4)
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2))
+  println(aSequence ++ Seq(5,6,7))
+  println(aSequence.sorted)
+
+  // Ranges
+  val aRange: Seq[Int] = 1 until 10
+  aRange.foreach(println)
+
+  (1 to 10). foreach(x => println("Hello"))
+
+  // Lists
+  val aList = List(1,2,3)
+  val prepended = 42 :: aList
+  println(prepended)
+
+  val appended = aList :+ 85
+  println(appended)
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+  println(aList.mkString("-|-"))
+
+  // Arrays
+  val numbers = Array(1,2,3,4)
+  val threeElements = Array.ofDim[String](3)
+  threeElements.foreach(println)
+
+  // Mutations
+  numbers(2) = 0 // syntax sugar for numbers.update(2,0)
+  println(numbers.mkString(" "))
+
+  // Arrays and Seq
+  val numbersSeq: Seq[Int] = numbers // implicit conversion
+  println(numbersSeq)
+
+  // Vectors
+  val vector: Vector[Int] = Vector(1,2,3)
+  println(vector)
+
+  // Vectors vs lists
+
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt())
+      System.nanoTime() - currentTime
+    }
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numbersList = (1 to maxCapacity).toList
+  val numbersVector = (1 to maxCapacity).toVector
+
+  // keeps reference to tail
+  // updating an element in the middle takes long
+  println(getWriteTime(numbersList))
+  // depth of the tree is small
+  // needs to replace an entire 32-element chunk
+  println(getWriteTime(numbersVector))
+
+
+
+
+}
